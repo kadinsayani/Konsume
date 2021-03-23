0d5c9c76015ee51b722aa5f283d552c23795b695
Kadin Sayani <kadinsayani8@gmail.com>
2021-03-22 19:39:31 -0600

    added textinput listener, button with test alert

--- App.js
+++ App.js
@@ -1,15 +1,24 @@
-import React from 'react';
-import {SafeAreaView, Text, TextInput} from 'react-native';
+import React, {useState} from 'react';
+import {SafeAreaView, Text, TextInput, Button, Alert} from 'react-native';
 
 const RecordEntry = () => {
+  const [input, setInput] = useState('');
   return (
     <SafeAreaView>
       <Text>
         Welcome to Konsume! Please enter an amount and a food item below.
       </Text>
-      <TextInput></TextInput>
+      <TextInput
+        placeholder="Enter food item here"
+        onChangeText={input => setInput(input)}
+        defaultValue={input}></TextInput>
+      <Button title="Submit" onPress={() => Alert.alert(input)}></Button>
     </SafeAreaView>
   );
+};
+
+const apiRequest = input => {
+  // todo - api request and parse
 };
 
 const App = () => {
